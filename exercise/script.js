1c52702a57159afabebb61b26d56fe083729a795
Thai Le Q <thai.le@asiantech.vn>
2018-06-26 21:14:08 +0700

    baitap js

---
+++ exercise/script.js
@@ -0,0 +1,44 @@
+function baitap1(a,b){
+  if(a==b) return (a+b)*3;
+  return a+b;
+}
+function baitap2(a) {
+  if(a<=19) return 19-a;
+  return (a-19)*3
+}
+function baitap3(s1) { 
+  var mang = [];
+  var s2 = s1.replace("*","0");
+  console.log(s2);
+  var sum=0;
+  for(i=0;i<s2.length;i++){
+    sum = sum + Number(s2.charAt(i));
+  }
+  for(i=0;i<10;i++){
+    if((sum+i)%3==0){
+      var rs =s1.replace("*",i);
+      mang.push(rs);
+      console.log(rs);
+    }
+  }
+  return mang;
+}
+function baitap4(s1) { 
+  var mang = [];
+  var s2;
+  console.log(s2);
+  for(i=0;i<10;i++){
+    s2 = s1.replace("*",i);
+    var sum =0;
+    for(j=0;j<s2.length;j++){
+       sum = sum + Number(s2.charAt(j));
+    }
+    if((sum%3==0)){
+      console.log(s2);
+      mang.push(s2);
+    }
+  }
+  return mang;
+}
+
+
